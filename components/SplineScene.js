eebc43bce50ce4adf32c36fca5c9aa3889fd9872
Danny Trejo <danieltrejo112@gmail.com>
2023-08-01 22:12:30 -0700

    Added skeleton

--- components/SplineScene.js
+++ components/SplineScene.js
@@ -1,10 +1,19 @@
-import Spline from '@splinetool/react-spline'
+import React, { Suspense } from 'react';
+import { Skeleton } from '@mui/material';
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function SplineScene({isOpen}) {
     return (
+        <Suspense fallback={<Skeleton
+            sx={{ bgcolor: '#808080'}}
+            variant="rectangular"
+            width={'100%'}
+            height={'100%'}
+          />}> 
             <Spline 
                 className={ isOpen ? 'splineOpen' : 'spline'}
                 scene="https://prod.spline.design/93uYArLT1jhthCdH/scene.splinecode"
                  />
+        </Suspense>
     );
 }
